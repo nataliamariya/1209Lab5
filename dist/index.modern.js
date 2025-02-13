fd11cc124f0f989780d1d45cb20929a237c1164e
nataliamariya <nbohulev@uwo.ca>
2025-02-12 20:40:39 -0500

    npm changes

---
+++ dist/index.modern.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import $ from 'jquery';
+
+var styles = {"pane":"_19OqE","navbar":"_31s4L","paneContent":"_24V_p"};
+
+var TabbedPaneComponent = function TabbedPaneComponent(_ref) {
+  var data = _ref.data;
+  $(function () {
+    if (data !== undefined) {
+      console.log('data.contents.length: ' + data.contents.length);
+      $('#navbarDiv').empty();
+      for (var i = 0; i < data.contents.length; i++) {
+        var newDiv = $('<div class=' + styles.navbar + '></div>');
+        var newAnchor = $('<a class="paneLink"></a>').text(data.contents[i].subject).attr('data-text', data.contents[i].text);
+        newDiv.append(newAnchor);
+        newAnchor.click(function () {
+          $('#paneContentDiv').html($(this).data('text'));
+        });
+        $('#navbarDiv').append(newDiv);
+      }
+      if (data.contents.length > 0) {
+        $('.paneLink:first').click();
+      }
+    }
+  });
+  return /*#__PURE__*/React.createElement("div", {
+    id: "pane",
+    className: styles.pane
+  }, /*#__PURE__*/React.createElement("div", {
+    id: "navbarDiv"
+  }), /*#__PURE__*/React.createElement("div", {
+    id: "paneContentDiv",
+    className: styles.paneContent
+  }));
+};
+
+export default TabbedPaneComponent;
+//# sourceMappingURL=index.modern.js.map
